e6f6429011cfc25130dc4a25586fd33b7c1e02ff
Jack Scarpelli <Festivetomcat99@gmail.com>
2022-12-14 18:52:28 -0500

    commit tree 2feb9d0fa3dbbaebe846c220276659652e640a37

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+
+console.log(`Scuber ${companyName}`);
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+console.log(`The most profitable neighborhood is ${mostProfitableNeighborhood}`);
+
+let companyCeo = 'Susan Smith';
+
+console.log(`The CEO of Scuber is ${companyCeo}`);
